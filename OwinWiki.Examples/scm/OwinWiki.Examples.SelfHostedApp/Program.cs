d8fc7b2be2611fdd8dc98f1b0332eaa964fd7ba7
Shane Connon <shane.connon@gmail.com>
2015-06-24 21:54:08 -0700

    examples

--- OwinWiki.Examples/scm/OwinWiki.Examples.SelfHostedApp/Program.cs
+++ OwinWiki.Examples/scm/OwinWiki.Examples.SelfHostedApp/Program.cs
@@ -1,15 +1,51 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OwinWiki.Examples.SelfHostedApp
 {
-    class Program
+    public class Startup
     {
         static void Main(string[] args)
         {
+            using (WebApp.Start<Startup>("http://localhost:9000"))
+            {
+                Console.WriteLine("Launched site on http://localhost:9000");
+                Console.WriteLine("Press [space] to quit...");
+
+                while (Console.ReadKey(true).KeyChar != ' ') ;
+            }
+        }
+
+        public void Configuration(IAppBuilder app) 
+        {
+            // Middleware 1
+            // configure katana to display the OWIN 
+            // welcome page at url "/"
+            app.UseWelcomePage("/");
+
+            // Middleware 2
+            // configure a yellow screen of death
+            app.UseErrorPage(); 
+
+            // Middleware 3
+            // Our web framework
+            app.Run(context => 
+            { 
+                Trace.WriteLine(context.Request.Uri); 
+                
+                //Line to show the ErrorPage 
+                if (context.Request.Path.ToString().Equals("/throwexception")) 
+                    throw new Exception("You requested the wrong URL :)");
+                
+                context.Response.ContentType = "text/plain"; 
+                return context.Response.WriteAsync("Hello, world."); 
+            }); 
         }
     }
 }
