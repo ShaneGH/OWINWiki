c8ade602630720031b08a7547a156ea24e645f0f
Shane Connon <shane.connon@gmail.com>
2015-06-24 22:22:12 -0700

    example

---
+++ OwinWiki.Examples/scm/OwinWiki.Examples.IISHostedApp/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(OwinWiki.Examples.IISHostedApp.Startup))]
+
+namespace OwinWiki.Examples.IISHostedApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context => 
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
